0d0a44d82f5cc34c393cc660376ce3f1a0160bc0
rangsan.chi <rangsan.chi@bla.co.th>
2023-12-14 17:19:15 +0700

    add apiweb

--- SaleContractAPI.BusinessLogic/ServiceAction.cs
+++ SaleContractAPI.BusinessLogic/ServiceAction.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using SaleContractAPI.DataAccess;
+using SaleContractAPI.DataContract;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SaleContractAPI.BusinessLogic
 {
     public class ServiceAction
     {
+        #region " STATIC "
         private readonly string _connectionstring = string.Empty;
         private readonly CultureInfo culture = new CultureInfo("th-TH");
         private readonly string DBENV = string.Empty;
@@ -15,5 +19,140 @@ namespace SaleContractAPI.BusinessLogic
             this._connectionstring = connectionstring;
             this.DBENV = DBENV;
         }
+        #endregion " STATIC "
+
+        #region " GET "
+        public async ValueTask<List<substatus>> GET_STATUS(string status_type)
+        {
+            List<substatus> dataObjects = null;
+            Repository repository = new Repository(_connectionstring, DBENV);
+            await repository.OpenConnectionAsync();
+            try
+            {
+                dataObjects = await repository.GET_STATUS(status_type);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                await repository.CloseConnectionAsync();
+            }
+            return dataObjects;
+        }
+
+        public async ValueTask<List<Priority>> GET_PRIORITY()
+        {
+            List<Priority> dataObjects = null;
+            Repository repository = new Repository(_connectionstring, DBENV);
+            await repository.OpenConnectionAsync();
+            try
+            {
+                dataObjects = await repository.GET_PRIORITY();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                await repository.CloseConnectionAsync();
+            }
+            return dataObjects;
+        }
+        public async ValueTask<List<TBT_SALE_STATUS>> GET_TBT_SALE_STATUS(int company_id)
+        {
+            List<TBT_SALE_STATUS> dataObjects = null;
+            Repository repository = new Repository(_connectionstring, DBENV);
+            await repository.OpenConnectionAsync();
+            try
+            {
+                dataObjects = await repository.GET_TBT_SALE_STATUS(company_id);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                await repository.CloseConnectionAsync();
+            }
+            return dataObjects;
+        }
+
+        public async ValueTask<List<company_detail>> GET_COMPANY(SEARCH_COMPANY condition)
+        {
+            List<company_detail> dataObjects = null;
+            Repository repository = new Repository(_connectionstring, DBENV);
+            await repository.OpenConnectionAsync();
+            try
+            {
+                dataObjects = await repository.GET_COMPANY(condition);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                await repository.CloseConnectionAsync();
+            }
+            return dataObjects;
+        }
+
+        #endregion " GET "
+
+
+        #region " INSERT "
+        public async ValueTask INSERT_TBT_COMPANY_DETAIL(company_detail condition)
+        {
+            Repository repository = new Repository(_connectionstring, DBENV);
+            await repository.OpenConnectionAsync();
+            await repository.beginTransection();
+            try
+            {
+                await repository.INSERT_TBT_COMPANY_DETAIL(condition);
+                await repository.CommitTransection();
+            }
+            catch (Exception ex)
+            {
+                await repository.RollbackTransection();
+                throw ex;
+
+            }
+            finally
+            {
+                await repository.CloseConnectionAsync();
+            }
+        }
+
+        public async ValueTask INSERT_TBT_SALE_STATUS(TBT_SALE_STATUS condition)
+        {
+            Repository repository = new Repository(_connectionstring, DBENV);
+            await repository.OpenConnectionAsync();
+            await repository.beginTransection();
+            try
+            {
+                await repository.INSERT_TBT_SALE_STATUS(condition);
+                await repository.CommitTransection();
+            }
+            catch (Exception ex)
+            {
+                await repository.RollbackTransection();
+                throw ex;
+
+            }
+            finally
+            {
+                await repository.CloseConnectionAsync();
+            }
+        }
+
+        #endregion " INSERT "
     }
 }
--- SaleContractAPI/Controllers/ManagesController.cs
+++ SaleContractAPI/Controllers/ManagesController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SaleContractAPI.BusinessLogic;
+using SaleContractAPI.DataContract;
 
 namespace SaleContractAPI.Controllers
 {
+    [Authorize]
     [Route("api/v1/[controller]")]
     [ApiController]
     public class ManagesController : ControllerBase
@@ -13,7 +17,87 @@ namespace SaleContractAPI.Controllers
         {
             this.Configuration = Configuration;
             this.service = new ServiceAction(this.Configuration.GetConnectionString("ConnectionSQLServer"), Configuration["ConfigSetting:DBENV"]);
-            //this.mailService = mailService;
         }
+
+        [HttpGet("GET_STATUS/{status_type}")]
+        [AllowAnonymous]
+        public async ValueTask<IActionResult> GET_STATUS(string status_type)
+        {
+            try
+            {
+                var result = await this.service.GET_STATUS(status_type);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpGet("GET_PRIORITY")]
+        [AllowAnonymous]
+        public async ValueTask<IActionResult> GET_PRIORITY()
+        {
+            try
+            {
+                var result = await this.service.GET_PRIORITY();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpGet("GET_TBT_SALE_STATUS/{companyid}")]
+        [AllowAnonymous]
+        public async ValueTask<IActionResult> GET_TBT_SALE_STATUS(int companyid)
+        {
+            try
+            {
+                var result = await this.service.GET_TBT_SALE_STATUS(companyid);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpPost("GET_COMPANY")]
+        [AllowAnonymous]
+        public async ValueTask<IActionResult> GET_COMPANY(SEARCH_COMPANY condition)
+        {
+            try
+            {
+                var result = await this.service.GET_COMPANY(condition);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpPost("INSERT_TBT_COMPANY_DETAIL")]
+        [AllowAnonymous]
+        public async ValueTask<IActionResult> INSERT_TBT_COMPANY_DETAIL(company_detail condition)
+        {
+            try
+            {
+                await this.service.INSERT_TBT_COMPANY_DETAIL(condition);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
     }
 }
